671f846302f8ee467eb65f1f9ca435d352368ff1
Daniel Pipkin <danielpipkin@centerlines-mbp.home>
2020-05-04 12:41:24 -0400

    edits copy according to finalized copy doc // minor styles to banner

--- src/js/app.js
+++ src/js/app.js
@@ -5,23 +5,29 @@ import 'bootstrap';
 import utils from './utils.js';
 import '../css/style.scss';
 
+let rangeValues = [];
+
 let rangeSlider = function () {
     let slider = $('.range-slider'),
         range = $('.range-slider__range'),
         value = $('.range-slider__value');
 
     slider.each(function () {
-
-        value.each(function () {
-            let value = $(this).prev().attr('value');
-            $(this).html('$' + utils.commaSeparateNumber(value));
-            addAll();
-        });
-
         range.on('input', function () {
-            $(this).next(value).html('$' + utils.commaSeparateNumber(this.value));
+            $(this).next(value).html(utils.commaSeparateNumber(this.value));
             addAll();
         });
+    });
+
+    value.each(function () {
+        let value = $(this).prev().attr('value');
+        let id = $(this).prev().attr('id');
+
+        $(this).html(utils.commaSeparateNumber(value));
+        // addAll();
+        console.log('id:', id, 'value:', value)
+        rangeValues.push(id, value);
+        console.log('range values', rangeValues)
     });
 };
 
@@ -31,19 +37,23 @@ let addAll = function () {
         sum += isNaN(this.value) || $.trim(this.value) === '' ? 0 : parseFloat(this.value);
     });
     $('#total-annual-value').html('$' + utils.commaSeparateNumber(sum));
+
 }
 
-$('#assessment-cta').on('click', function () {
-    $('#assessment-wrap').hide();
-    $('.main-banner').addClass('results');
-    $('#results-wrap').show();
-    $('html, body').animate({ scrollTop: '0px' }, 500);
-})
+// $('#assessment-cta').on('click', function () {
+//     $('#assessment-wrap').hide();
+//     $('.main-banner').addClass('results');
+//     $('#results-wrap').show();
+//     $('html, body').animate({ scrollTop: '0px' }, 500);
+// })
 
 $(document).ready(function () {
-    addAll();
+
     rangeSlider();
+    addAll();
+
     $(function () {
         $('[data-toggle="tooltip"]').tooltip()
-    })
+    });
+
 });
