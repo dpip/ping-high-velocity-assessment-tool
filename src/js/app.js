48f9fc882c792f904e06d7fe9118ace15ba30071
Daniel Pipkin <danielpipkin@centerlines-mbp.home>
2020-05-04 20:18:23 -0400

    new build with productivity sum complete and security in progress

--- src/js/app.js
+++ src/js/app.js
@@ -8,15 +8,24 @@ import '../css/style.scss';
 let rangeValues = [];
 
 let rangeSlider = function () {
+    let r = rangeValues;
     let slider = $('.range-slider'),
         range = $('.range-slider__range'),
         value = $('.range-slider__value');
 
     slider.each(function () {
         range.on('input', function () {
+            let id = utils.parseNumber($(this).attr('id'));
+            let updatedValue = Number(this.value);
             $(this).next(value).html(utils.commaSeparateNumber(this.value));
-            addAll();
+            // addAll();
+            rangeValues[id] = updatedValue;
+            console.log('from slider', updatedValue);
+            $('#cat1').html('$' + `${calcProductivity(rangeValues)}`);
+            $('#cat2').html('$' + `${calcSecurity(rangeValues)}`);
+
         });
+
     });
 
     value.each(function () {
@@ -25,11 +34,50 @@ let rangeSlider = function () {
 
         $(this).html(utils.commaSeparateNumber(value));
         // addAll();
-        console.log('id:', id, 'value:', value)
-        rangeValues.push(id, value);
-        console.log('range values', rangeValues)
+        rangeValues.push(Number(value));
+
+        $('#cat1').html('$' + `${calcProductivity(rangeValues)}`);
+        $('#cat2').html('$' + `${calcSecurity(rangeValues)}`);
     });
 };
+
+let calcProductivity = function (rangeValues) {
+    let r = rangeValues;
+    let p0 = utils.productivityBasic(r[0], r[7])
+    let p1 = utils.p1(r[0], r[7]);
+    let p2 = utils.p2(r[2], r[3], r[6], r[7]);
+    let p3 = utils.p3(r[2], r[6]);
+    let p4 = utils.p4(r[6], r[5]);
+    let p5 = utils.p5(r[0], r[7]);
+    // console.log('from calc', r);
+    let sumProductivity = Number(p0 + p1 + p2 + p3 + p4 + p5);
+
+    return utils.commaSeparateNumber(sumProductivity);
+}
+
+let calcSecurity = function (rangeValues) {
+    let r = rangeValues;
+    // let s0 = utils.securityBasic(r[0], r[3], r[4], r[2], r[5]);
+
+    // console.log('from calc', r);
+    // let sumProductivity = Number(s0 + s1 + s2, s3, s4, s5);
+
+    // return utils.commaSeparateNumber(sumSecurity);
+}
+
+// let calcAgility = function (rangeValues) {
+//     let r = rangeValues;
+//     let p0 = utils.productivityBasic(r[0], r[7])
+//     let p1 = utils.p1(r[0], r[7]);
+//     let p2 = utils.p2(r[2], r[3], r[6], r[7]);
+//     let p3 = utils.p3(r[2], r[6]);
+//     let p4 = utils.p4(r[6], r[5]);
+//     let p5 = utils.p5(r[0], r[7]);
+//     // console.log('from calc', r);
+//     let sumProductivity = Number(p0 + p1 + p2 + p3 + p4 + p5);
+
+//     return utils.commaSeparateNumber(sumProductivity);
+// }
 
 let addAll = function () {
     let sum = 0
@@ -40,20 +88,11 @@ let addAll = function () {
 
 }
 
-// $('#assessment-cta').on('click', function () {
-//     $('#assessment-wrap').hide();
-//     $('.main-banner').addClass('results');
-//     $('#results-wrap').show();
-//     $('html, body').animate({ scrollTop: '0px' }, 500);
-// })
-
 $(document).ready(function () {
 
     rangeSlider();
     addAll();
-
     $(function () {
         $('[data-toggle="tooltip"]').tooltip()
     });
-
 });
--- src/js/utils.js
+++ src/js/utils.js
@@ -20,7 +20,39 @@ const utils = {
         while (/(\d+)(\d{3})/.test(val.toString())) {
             val = val.toString().replace(/(\d+)(\d{3})/, '$1' + ',' + '$2');
         }
+        console.log(val)
         return val;
+    }, parseNumber: function (str) {
+        return Number(str.split('-')[1]);
+    }, productivityBasic: function (a, b) {
+        // console.log('from utils', Number(Math.round(a * b * 10.93 * 0.9947643979)));
+        return Number(Math.round(((a * b) * 10.93) * 0.9947643979));
+    }, p1: function (a, b) {
+        return Number(Math.round((a * 7.35) * (b * 0.3333333333333)));
+    }, p2: function (a, b, c, d) {
+        return Number((a + b) * 1.5) * ((2.5 * c) + (8 * d));
+    }, p3: function (a, b) {
+        return (Number(a * 8) * b);
+    }, p4: function (a, b) {
+        return (Number(a * 500) * b);
+    }, p5: function (a, b) {
+        return (Number((a * 12.5) * 0.95) * b);
+    }, securityBasic: function (a) {
+        return (((a * 10) * 0.416) * 0.296) * 0.999;
+    }, s1: function (a) {
+        return (a * 0.04) * 0.9;
+    }, s2: function (a, b, c, d, e) {
+        let step1 = ((a * 147) / 60);
+        let step2 = ((b + c) * 12);
+        let step3 = (c * 6);
+        let step4 = (d * 500);
+        let step5 = (step1 + step2 + step3 + step4);
+        let step6 = (step4 / 2000);
+        let step7 = Math.min(step5, step6) * 10;
+        let step8 = (step7 * 0.1);
+
+        return (step7 * step8);
+
     }
 };
 
